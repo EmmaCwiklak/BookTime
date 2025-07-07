75bcad681830898f305599e0dc7f0ebc8b19c9d3
Emma Ćwiklak <103240822+EmmaCwiklak@users.noreply.github.com>
2025-07-07 19:29:38 +0200

    Update FirebaseManager.swift

--- BookTime/BookTime/ProfileView/FirebaseManager.swift
+++ BookTime/BookTime/ProfileView/FirebaseManager.swift
@@ -1,31 +1,7 @@
 import FirebaseFirestore
 import FirebaseAuth
 
-//class FirebaseManager: ObservableObject {
-//    private let db = Firestore.firestore()
-//    private var userId: String? {
-//        Auth.auth().currentUser?.uid
-//    }
-//
-//    func toggleBookStatus(book: Book, collection: String) {
-//        guard let userId = userId else { return }
-//        let ref = db.collection("users").document(userId).collection(collection).document(book.id)
-//
-//        ref.getDocument { (document, error) in
-//            if let document = document, document.exists {
-//                ref.delete() // Jeśli książka jest, usuwamy
-//            } else {
-//                ref.setData([
-//                    "title": book.title,
-//                    "author": book.authors?.joined(separator: ", ") ?? "Nieznany",
-//                    "cover": book.imageLinks?.thumbnail ?? "",
-//                    "pageCount": book.pageCount ?? 0
-//                ]) // Jeśli nie ma, dodajemy
-//            }
-//        }
-//    }
-//
-//}
+
 class FirebaseManager: ObservableObject {
     private let db = Firestore.firestore()
     private var userId: String? {
